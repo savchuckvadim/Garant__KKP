dc18f491de2c1ed031fd8eca76c3f49d42cfd511
savchuckvadim <savchuckvadim@gmail.com>
2022-10-05 17:50:34 +0300

    fixed allPrices-reducer

--- src/modules/redux/redusers/all-price-reducer.js
+++ src/modules/redux/redusers/all-price-reducer.js
@@ -1,27 +1,21 @@
 import { pricesAPI } from "../../services/api"
-import { coefficientsAPI } from "../../services/classic-coefficients-api"
 
 const SET_PRICES = 'SET_PRICES'
-const SET_COEFFICIENTS = 'SET_COEFFICIENTS'
+
+
+
 const initialState = {
     classics: {
         localPrices: [],
-        coefficients: []
+
     },
-    prof: {
-        OD1: [],
-        OD2: [],
-        OD3: [],
-        OD5: [],
-        OD10: [],
-        OD20: [],
-        OD30: [],
-        OD50: [],
-    }
+    prof: [],
+    kmv: [],
+    stv: [],
+    coefficients: []
 }
 //AC
 const setPrices = (prices) => ({ type: SET_PRICES, prices })
-const setCoefficients = (coefficients) => ({ type: SET_COEFFICIENTS, coefficients })
 // [5400, 5400, 7776, 7776, 6804, 6912, 10908, 13608],
 // [6480, 6480, 9396, 9396, 8208, 8316, 13068, 16308],
 
@@ -33,21 +27,21 @@ export const getPrices = () => async (dispatch) => {
     dispatch(setPrices(prices))
 }
 
-const getCoefficients = () => async (dispatch) => {
 
-    const coefficients = await coefficientsAPI.getCoefficients()
-    dispatch(setCoefficients(coefficients))
-}
 
 const allPriceReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case SET_PRICES:
 
-            return state;
+            return {
+                ...state, prof: action.prices.prof,
+                coefficients: action.prices.coefficients,
+                kmv: action.prices.kmv,
+                stv: action.prices.stv,
+            };
 
-        case SET_COEFFICIENTS:
-            return { ...state, coefficients: action.coefficients }
+
         default:
             return state;
     }
