68504ad06009180e7c43838cd5d2caf61ac48500
savchuckvadim <savchuckvadim@gmail.com>
2022-10-07 12:58:06 +0300

    refactoring type of contract width global supply

--- src/modules/redux/redusers/deal/typeOfContract-reducer.js
+++ src/modules/redux/redusers/deal/typeOfContract-reducer.js
@@ -1,3 +1,5 @@
+import { SET_SUPPLY } from "../global-parameters/global-parameters-reducer";
+
 let TYPE_OF_CONTRACT = 'TYPE_OF_CONTRACT';
 
 let initialState = {
@@ -15,42 +17,79 @@ let initialState = {
         }
     },
     typesOfContract: [{
-            id: '1913',
-            name: 'Internet',
-            typeOfGood: 'prof',
-            units: {
-                value: 'Месяц',
-                ID: '11',
-                CODE: '15',
-            }
+        id: '1913',
+        name: 'Internet',
+        typeOfGood: 'prof',
+        units: {
+            value: 'Месяц',
+            ID: '11',
+            CODE: '15',
+        }
 
 
-        },
-        // {
-        //     id: '1915',
-        //     name: 'Договор услуг',
-        //     typeOfGood: 'prof'
-        // },
-        {
-            id: '3616',
-            name: 'Абонентский полгода',
-            typeOfGood: 'abonSix',
-            units: {
-                value: 'Абон. 6 мес.',
-                ID: '12',
-                CODE: '20',
-            }
-        },
-        {
-            id: '3616',
-            name: 'Абонентский год',
-            typeOfGood: 'abonEleven',
-            units: {
-                value: 'Абон. 12 мес.',
-                ID: '14',
-                CODE: '25',
-            }
-        },
+    },
+  
+    {
+        id: '3616',
+        name: 'Абонентский полгода',
+        typeOfGood: 'abonSix',
+        units: {
+            value: 'Абон. 6 мес.',
+            ID: '12',
+            CODE: '20',
+        }
+    },
+    {
+        id: '3616',
+        name: 'Абонентский год',
+        typeOfGood: 'abonEleven',
+        units: {
+            value: 'Абон. 12 мес.',
+            ID: '14',
+            CODE: '25',
+        }
+    },
+
+
+    ],
+    internetTypesOfContract: [{
+        id: '1913',
+        name: 'Internet',
+        typeOfGood: 'prof',
+        units: {
+            value: 'Месяц',
+            ID: '11',
+            CODE: '15',
+        }
+    },
+
+    {
+        id: '3616',
+        name: 'Абонентский полгода',
+        typeOfGood: 'abonSix',
+        units: {
+            value: 'Абон. 6 мес.',
+            ID: '12',
+            CODE: '20',
+        }
+    },
+    {
+        id: '3616',
+        name: 'Абонентский год',
+        typeOfGood: 'abonEleven',
+        units: {
+            value: 'Абон. 12 мес.',
+            ID: '14',
+            CODE: '25',
+        }
+    }],
+    proximaTypesOfContract: [
+    {
+        id: '1915',
+        name: 'Договор услуг',
+        typeOfGood: 'prof'
+    },
+    
         // {
         //     id: '3170',
         //     name: 'Лицензионный',
@@ -60,6 +99,7 @@ let initialState = {
     ]
 
 }
+
 
 
 
@@ -77,14 +117,26 @@ const changeTypeOfContract = (stateCome, action) => {
         ...stateCome
     }
     state.value = state.typesOfContract[action.index]
-  
+
     return state
 }
 
 export const typeOfContractReducer = (state = initialState, action) => {
 
-    if (action.type === TYPE_OF_CONTRACT) {
-        return changeTypeOfContract(state, action)
+    switch (action.type) {
+        case TYPE_OF_CONTRACT:
+
+            return changeTypeOfContract(state, action)
+
+        case SET_SUPPLY: //from global-parameters-reducer    
+            if (action.index === 1) {
+                return { ...state, typesOfContract: state.internetTypesOfContract }
+            } else if (action.index === 0) {
+                return { ...state, typesOfContract: state.proximaTypesOfContract }
+            }
+            return state
+        default:
+            return state;
     }
-    return state
+   
 }
